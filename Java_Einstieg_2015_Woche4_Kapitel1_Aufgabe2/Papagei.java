21ac15cdb160865f1f8a6cea87750f3c9811e6fc
Kevin Sommer <Kevin.Sommer91@freenet.de>
2021-12-15 14:26:52 +0100

    Woche 4, Aufgabe 2 clean hinzugefuegt

---
+++ Java_Einstieg_2015_Woche4_Kapitel1_Aufgabe2/Papagei.java
@@ -0,0 +1,11 @@
+public class Papagei {
+    String name = "";
+ 
+    public void setName(String name) {
+        this.name = name;
+    }   
+    
+    public String gibSatz() {
+        return "Hallo, ich bin " + name;
+    }
+}
---
+++ Java_Einstieg_2015_Woche4_Kapitel1_Aufgabe2/Zoo.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class Zoo {
+    // Lege hier das Attribut papageien an:
+    
+    public Zoo() {
+        Papagei alex = new Papagei();
+        alex.setName("Alex");
+        Papagei jaspar = new Papagei();
+        jaspar.setName("Jaspar");
+        Papagei kiki = new Papagei();
+        kiki.setName("Kiki");
+        // Fuelle hier die Map
+    }
+    
+    public HashMap getPapageien() {
+        return papageien;
+    }
+    
+    public static void main(String args[]) {
+        Zoo zoo = new Zoo();
+        if (zoo.getPapageien().get("Alex") != null) {
+            System.out.println("Es gibt einen Papagei mit dem Namen Alex");
+        }
+        if (zoo.getPapageien().get("Berta") == null) {
+            System.out.println("Im Zoo lebt kein Papagei mit den Namen Berta");
+        }
+    }
+}
